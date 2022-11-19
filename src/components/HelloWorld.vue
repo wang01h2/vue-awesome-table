5def7775769b90fdd56ea1c7c03663fe34854c68
wang01h2 <1299839398@qq.com>
2022-11-19 11:09:38 +0800

    feat: 🚀自动引入组件

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,14 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
 <template>
   <h1>{{ msg }}</h1>
-
+  <a-button>Test</a-button>
   <div class="card">
     <button type="button" @click="count++">
       count is {{ count }}
